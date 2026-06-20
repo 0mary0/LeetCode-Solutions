b91c34c3d21640ecf28666fb622ee616b928de22
0mary0 <mary.ashnena@gmail.com>
2026-06-20 20:08:42 +0330

    Create Climbing Staris.py

---
+++ Easy/Climbing Staris.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def __init__(self):
+        self.memo = {1: 1, 2: 2}
+    def climbStairs(self, n):
+        if n in self.memo:
+            return self.memo[n]
+        else:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+            return self.memo[n]
